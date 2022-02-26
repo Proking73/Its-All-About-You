30fe7e0c1ae85b24de2d2e799b554143d13f17f9
77035338 <MY_NAME@example.com>
2022-02-26 18:33:05 -0500

    save

--- script.js
+++
@@ -1,7 +0,0 @@
-const options = document.querySelectorAll(".options");
-
-options.forEach(element => {
-    element.addEventListener('click', () => {
-        element.style.opacity = "0";
-    });
-});
